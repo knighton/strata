af7c3033991d2c95681d9d1f6d3b4d6e2b5e4fcb
James Knighton <iamknighton@gmail.com>
2019-02-20 14:19:09 -0800

    Fix <= bug.

--- src/io/str/reader.cc
+++ src/io/str/reader.cc
@@ -15,7 +15,7 @@ StrataStringReader::StrataStringReader(const string* data, size_t* index) {
 
 bool StrataStringReader::ReadRaw(uint32_t size, char* bytes,
                                  uint32_t* bytes_read) {
-    if (data_->size() <= *index_ + size) {
+    if (data_->size() < *index_ + size) {
         return false;
     }
     memcpy(bytes, &(*data_)[*index_], size);

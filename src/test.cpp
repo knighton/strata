5a2fc6df91a10e953a7451a604133c3710fc4425
James Knighton <iamknighton@gmail.com>
2019-02-20 16:02:42 -0800

    Buffer writing.

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <utility>
 
+#include "io/buf/func.h"
+#include "io/buf/reader.h"
+#include "io/buf/writer.h"
 #include "io/str/func.h"
 #include "io/str/reader.h"
 #include "io/str/writer.h"
@@ -11,6 +14,7 @@
 using std::make_pair;
 using std::pair;
 using namespace strata::io::base;
+using namespace strata::io::buf;
 using namespace strata::io::str;
 using strata::util::InitCRC32;
 using std::string;
@@ -38,7 +42,79 @@ void DumpVector(const vector<string>& items) {
     printf("]\n");
 }
 
-void Test(const StrataWriteFlags& flags, const vector<string>& items) {
+struct Buffer {
+    char* data{nullptr};
+    const char* data_begin{nullptr};
+    const char* data_end{nullptr};
+
+    Buffer();
+    ~Buffer();
+
+    void ToString(string* s) const;
+
+    bool EqualsString(const string& s) const;
+};
+
+Buffer::Buffer() {
+    data = new char[2048];
+    assert(data);
+    data_begin = data;
+    data_end = &data[2048];
+}
+
+Buffer::~Buffer() {
+    if (data) {
+        delete [] data_begin;
+    }
+}
+
+void Buffer::ToString(string* s) const {
+    size_t data_size = static_cast<size_t>(data - data_begin);
+    *s = string(data_begin, data_size);
+}
+
+bool Buffer::EqualsString(const string& s) const {
+    string t;
+    ToString(&t);
+    return s == t;
+}
+
+void TestBufferIO(const StrataWriteFlags& flags, const vector<string>& items) {
+    string data;
+    {
+        Buffer buffer;
+        for (auto& item : items) {
+            assert(WriteOneToStrataBuffer(flags, item, &buffer.data,
+                                          buffer.data_end));
+        }
+        buffer.ToString(&data);
+    }
+
+    {
+        Buffer buffer;
+        assert(WriteToStrataBuffer(flags, items, &buffer.data,
+                                   buffer.data_end));
+        assert(buffer.EqualsString(data));
+    }
+
+    {
+        Buffer buffer;
+        StrataBufferWriter writer(&buffer.data, buffer.data_end);
+        for (auto& item : items) {
+            assert(writer.WriteOne(flags, item));
+        }
+        assert(buffer.EqualsString(data));
+    }
+
+    {
+        Buffer buffer;
+        StrataBufferWriter writer(&buffer.data, buffer.data_end);
+        assert(writer.Write(flags, items) == items.size());
+        assert(buffer.EqualsString(data));
+    }
+}
+
+void TestStringIO(const StrataWriteFlags& flags, const vector<string>& items) {
     string data;
     for (auto& item : items) {
         assert(WriteOneToStrataString(flags, item, &data));
@@ -125,7 +201,8 @@ int main() {
             StrataWriteFlags flags;
             flags.snappy = snappy;
             flags.crc32 = crc32;
-            Test(flags, items);
+            TestBufferIO(flags, items);
+            TestStringIO(flags, items);
         }
     }
 }
